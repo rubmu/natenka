aa3bceecda486249769ebbc5b8c3757a33a6c1f4
Наташа Самойленко <nataliya.samoylenko@gmail.com>
2017-09-24 20:34:33 +0300

    Create dict_examples.py

---
+++ code_examples/colored_dict/dict_examples.py
@@ -0,0 +1,37 @@
+int_d = {1000:{10:100, 20:200},
+         2000:{10:100, 20:200},
+         3000:{10:100, 20:200},
+         4000:{100:{200:2, 300:3}}}
+
+
+london_co = {
+    'r1' : {
+    'hostname': 'london_r1',
+    'location': '21 New Globe Walk',
+    'vendor': 'Cisco',
+    'model': '4451',
+    'ios': '15.4',
+    'ip': {'fa0/0':'10.255.0.1',
+           'fa0/1':'10.255.1.1',
+           'fa0/2':'10.255.2.1'}
+    },
+    'r2' : {
+    'hostname': 'london_r2',
+    'location': '21 New Globe Walk',
+    'vendor': 'Cisco',
+    'model': '4451',
+    'ios': '15.4',
+    'ip': {'fa0/0':'10.255.11.1',
+           'fa0/1':'10.255.12.1',
+           'fa0/2':'10.255.13.1'}
+    },
+    'sw1' : {
+    'hostname': 'london_sw1',
+    'location': '21 New Globe Walk',
+    'vendor': 'Cisco',
+    'model': '3850',
+    'ios': '3.6.XE',
+    'ip': ['10.255.0.101', '10.255.2.101']
+    }
+}
+
